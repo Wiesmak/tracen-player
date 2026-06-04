3da00f84efe1bd15377cf7f46092427a2ad995b0
Gerard Gajda <gerard.gajda@gmail.com>
2026-06-04 14:39:52 +0200

    Landing screen animations & hi-dpi support

--- src/app/quiz/list/page.tsx
+++ src/app/quiz/list/page.tsx
@@ -1,7 +1,13 @@
+import { ViewTransition } from "react"
 import QuizCatalog from "@/features/quiz/components/QuizCatalog"
 
 export default function Home() {
     return (
-        <QuizCatalog />
+        <ViewTransition
+            enter={{'fade-white': 'fade-white-enter', default: 'none'}}
+            exit={{'fade-white': 'fade-white-exit', default: 'none'}}
+        >
+            <QuizCatalog/>
+        </ViewTransition>
     )
 }
